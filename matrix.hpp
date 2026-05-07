dbcddf70cbfa868187b87e76b1c41ab30b5ad8e5
jwihardi <allarkvarkk@gmail.com>
2026-05-06 20:04:56 -0400

    added tranpose (hopefully works)

--- matrix.hpp
+++ matrix.hpp
@@ -10,7 +10,7 @@ struct Matrix{
 
     Matrix(int rows_, int cols_) : data(rows_ * cols_) ,rows(rows_), cols(cols_) {}
     
-    Matrix Mult(const Matrix& other) const{
+    Matrix multiply(const Matrix& other) const{
         if(cols != other.rows) 
             throw std::runtime_error("Invalid dimensions for matrix multiplication");
 
@@ -22,6 +22,17 @@ struct Matrix{
                 for(int k = 0; k < cols; k++)
                     sum += data[i * cols + k] * other.data[k * other.cols + j];
                 res.data[i * res.cols + u] = sum;
+            }
+        }
+        return res;
+    }
+
+    Matrix transpose() const{
+        Matrix res(cols, rows);
+
+        for(int i = 0; i < rows; i++){
+            for(int u = 0; u < cols; u++){
+                res.data[u * res.cols + i] = data[i * cols + u];
             }
         }
         return res;
